06fef9813af569e331fa93ef7e08de1cf6d1c582
Ysmari <mendoza.ysmari@gmail.com>
2023-07-18 17:19:11 -0400

    aqui hice modificaciones en todos mis archivos, ya el proyecto funciona, me falta mejorar el style css

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,22 +1,38 @@
 const analyzer = {  
-  getWordCount: (text) => {
+
+  getWordCount:(text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const words = text.trim().split(/\s+/);
+    return words.length;
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    return text.length;
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    const textWithoutSpacesAndPunctuation = text.replace(/[^\w\s]|_/g, "").replace(/\s+/g, "");
+    return textWithoutSpacesAndPunctuation.length;
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const words = text.trim().split(/\s+/).filter(word => word !== '');
+    const totalLength = words.reduce((acc, word) => acc + word.length, 0);
+    return totalLength / words.length;
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    return (text.match(/\d+/g) || []).length;
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    const numbers = text.match(/\d+/g) || [];
+    const sum = numbers.reduce((acc, num) => acc + parseInt(num), 0);
+    return sum;
   },
 };
+
+const prueba = "hola mundo";
+console.log(analyzer.getWordCount(prueba));
 
 export default analyzer;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+
+function updateStats() {
+  const textArea =document.querySelector('[name="user-input"]');
+  const text = textArea.value;
+
+  document.getElementById('word-count').textContent = 'Palabras: ' + analyzer.getWordCount(text);
+  document.getElementById('character-count').textContent = 'Caracteres: ' + analyzer.getCharacterCount(text);
+  document.getElementById('character-no-spaces-count').textContent = 'Caracteres Sin Espacios: ' + analyzer.getCharacterCountExcludingSpaces(text);
+  document.getElementById('number-count').textContent = 'Recuento de Numeros: ' + analyzer.getNumberCount(text);
+  document.getElementById('number-sum').textContent = 'Suma Numeros: ' + analyzer.getNumberSum(text);
+  document.getElementById('word-length-average').textContent = 'Promedio Longitud: ' + analyzer.getAverageWordLength(text).toFixed(2);
+}
+
+function clearInput() {
+  const textArea = document.querySelector('[name="user-input"]');
+  textArea.value = '';
+  updateStats();
+
+}
+
+console.log({updateStats});
+function onDOMContentLoaded() {
+
+  document.querySelector('[id="reset-button"]').addEventListener('click', clearInput);
+  document.querySelector('[name="user-input"]').addEventListener('input', updateStats);
+}
+
+document.addEventListener('DOMContentLoaded', onDOMContentLoaded);
